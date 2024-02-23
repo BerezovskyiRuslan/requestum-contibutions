1625fb0d6b88e5d7a0a7fc27ec3fbbd49965d723
BerezovskyiRuslan <berezovskyiruslanwork@gmail.com>
2024-02-23 12:05:26 +0200

    fix/sotred by shared contributors

--- server/src/common/sorts/repos.sort.ts
+++ server/src/common/sorts/repos.sort.ts
@@ -3,7 +3,7 @@ import { Repos } from 'src/types/interfaces/repos.interface';
 export const sortReposByLengthContributions = (repos: Repos[]): Repos[] => {
   let reposArray: Repos[] = repos.sort(
     (repoA: Repos, repoB: Repos) =>
-      repoB.contributions.length - repoA.contributions.length,
+      repoB.sharedContributors.length - repoA.sharedContributors.length,
   );
 
   reposArray = reposArray.splice(0, 5);
